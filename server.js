a19a974d67379add00928dca4241b3a1669e3cd0
zeh sobrinho <sobrinhosj@gmail.com>
2024-11-21 22:56:40 -0300

    Update server.js
    Atualizado cabeçalho servers com documentacão .env etc

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.json" assert { type: "json" };
 import express from "express";
 import routes from "./src/routes/postsRoutes.js";
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// Construindo a URI com variáveis do .env
+const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/?retryWrites=true&w=majority&appName=${process.env.MONGO_APPNAME}`;
 
 const app = express();
 app.use(express.static("uploads"))
